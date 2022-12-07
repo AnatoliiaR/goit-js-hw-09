319372deb1397389957a5cfd214f2e98d51d02bb
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-12-07 11:32:57 +0200

    222

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+}
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+
+function onBtnStart() {
+
+    timerId = setInterval(changeColorBody, 500);
+
+    function changeColorBody() {
+        refs.btnStart.setAttribute('disabled', '');
+        refs.body.style.backgroundColor = getRandomHexColor();
+    
+    }
+
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+}
+
+function onBtnStop() {
+    clearInterval(timerId);
+    refs.btnStart.removeAttribute('disabled', '');
+
+}
+

bc27f53c8259398ffcd996b99b5c36c7a0b6d691
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-12-09 12:18:23 +0200

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,7 +4,7 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 
-// const flatpickr = require("flatpickr");
+
 
 const refs = {
     input: document.querySelector('#datetime-picker'),
@@ -15,13 +15,13 @@ const refs = {
     second: document.querySelector('[data-seconds]'),
 }
 
-
-
-
 let dateTime = null;
 let intervalID = null;
 
-// const chosedDate = selectedDates[0];
+refs.btnStart.setAttribute('disabled', '');
+
+refs.btnStart.addEventListener('click', onStart);
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -30,56 +30,56 @@ const options = {
     onClose(selectedDates) {
         if (selectedDates[0] < new Date()) {
             refs.btnStart.setAttribute('disabled', '');
-            // window.alert('Please choose a date in the future');
+            selectedDates[0] = new Date();
             Notiflix.Report.failure(
-  'no',
-  'date',
-  'close',
-  {
-    width: '360px',
-    svgSize: '120px',
-  },
-);
-            return
+                'Error',
+                'Please choose a date in the future',
+                'close',
+                {
+                    width: '360px',
+                    svgSize: '120px',
+                }
+            );
+            
+            // return
         }
         else {
             refs.btnStart.removeAttribute('disabled');
             dateTime = selectedDates[0];
 
         }
-          
-  },
+    },
 };
-flatpickr(refs.input, options);
-
-refs.btnStart.addEventListener('click', onStart);
-
 
 function onStart() {
-    setInterval(() => {
+    refs.btnStart.setAttribute('disabled', '');
+    intervalID = setInterval(() => {
         const disDate = dateTime - new Date();
         // console.log(dateTime - new Date());
 
         if (disDate < 0) {
             clearInterval(intervalID);
+            refs.btnStart.removeAttribute('disabled');
             return
         }
-        
-        const timeComponents = convertMs(disDate);
+const timeComponents = convertMs(disDate);
         // console.log(timeComponents);
 
         refs.day.textContent = timeComponents.days;
         refs.hour.textContent = timeComponents.hours;
         refs.minute.textContent = timeComponents.minutes;
         refs.second.textContent = timeComponents.seconds;
+
     }, 1000);
-
-
-    
-    
 }
+        
+        
+flatpickr(refs.input, options);
 
 
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -99,13 +99,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-
-function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-}
-
